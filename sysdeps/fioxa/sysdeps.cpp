22e6085274a332c8c4c8a441d08c0099f22c6b65
Hayley Patton <theemacsshibe@gmail.com>
2026-06-02 22:00:40 +1000

    Fewer STUBs

--- sysdeps/fioxa/sysdeps.cpp
+++ sysdeps/fioxa/sysdeps.cpp
@@ -11,9 +11,11 @@ extern "C" {
 
   extern int fioxa_close(int);
   [[noreturn]] extern void fioxa_exit(int);
+  extern int fioxa_isatty(int);
   extern int fioxa_map(size_t, void**);
   extern int fioxa_open(const char*, int, mode_t, int*);
   extern int fioxa_read(int, void*, size_t, ssize_t*);
+  extern int fioxa_seek(int, off_t, int, off_t*);
   extern void fioxa_set_fs_reg(void*);
   extern int fioxa_unmap(void*, size_t);
   extern int fioxa_write(int, const void*, size_t);
@@ -32,16 +34,19 @@ namespace mlibc {
     return 0;
   }
   int sys_close(int fd) { return fioxa_close(fd); }
-  int sys_exit(int status) { fioxa_exit(status); __builtin_unreachable(); }
+  int sys_exit(int status) { fioxa_exit(status); }
   int sys_futex_wait(int *pointer, int expected, const struct timespec *timeout) { STUB("wait"); }
   int sys_futex_wake(int *pointer) { STUB("wake"); }
+  int sys_isatty(int fd) { return fioxa_isatty(fd); }
   int sys_open(const char* path, int flags, mode_t mode, int *fd) {
     return fioxa_open(path, flags, mode, fd);
   }
   int sys_read(int fd, void *buf, size_t count, ssize_t *bytes_read) {
     return fioxa_read(fd, buf, count, bytes_read);
   }
-  int sys_seek(int fd, off_t offset, int whence, off_t *new_offset) { STUB("seek"); }
+  int sys_seek(int fd, off_t offset, int whence, off_t *new_offset) {
+    return fioxa_seek(fd, offset, whence, new_offset);
+  }
   int sys_tcb_set(void *pointer) { fioxa_set_fs_reg(pointer); return 0; }
   int sys_vm_map(void *hint, size_t size, int prot, int flags, int fd, off_t offset, void **window) {
     if (fd != -1) return -ENOSYS;
