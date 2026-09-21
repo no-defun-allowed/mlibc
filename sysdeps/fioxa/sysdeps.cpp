243c4fd06e3c5bf18ed562b7c3b8bfdd6be96526
Hayley Patton <theemacsshibe@gmail.com>
2026-09-21 19:43:37 +1000

    spell Sysdeps differently I guess

--- sysdeps/fioxa/sysdeps.cpp
+++ sysdeps/fioxa/sysdeps.cpp
@@ -2,8 +2,11 @@
 #include <errno.h>
 #include <sys/mman.h>
 #include <sys/types.h>
+#include <mlibc/sysdeps.hpp>
+#include <mlibc/sysdep-traits.hpp>
 
-#define STUB(NAME) { sys_libc_log("Hit a stub " NAME); sys_libc_panic(); }
+
+#define STUB(NAME) { fioxa_log("Hit a stub " NAME); fioxa_panic(); }
 
 extern "C" {
   extern void fioxa_log(const char*);
@@ -24,43 +27,41 @@ extern "C" {
 }
 
 namespace mlibc {
-  void sys_libc_log(const char *message) { fioxa_log(message); }
-  [[noreturn]] void sys_libc_panic() { fioxa_panic(); }
+  void Sysdeps<LibcLog>::operator()(const char *message) { fioxa_log(message); }
+  [[noreturn]] void Sysdeps<LibcPanic>::operator()() { fioxa_panic(); }
   
-  int sys_anon_free(void *pointer, size_t size) { return fioxa_unmap(pointer, size); }
-  int sys_clock_get(int clock, time_t *seconds, long *nanoseconds) {
+  int Sysdeps<AnonFree>::operator()(void *pointer, size_t size) { return fioxa_unmap(pointer, size); }
+  int Sysdeps<ClockGet>::operator()(int clock, time_t *seconds, long *nanoseconds) {
     return fioxa_clock(seconds, nanoseconds);
   }
-  int sys_close(int fd) { return fioxa_close(fd); }
-  int sys_exit(int status) { fioxa_exit(status); }
-  int sys_futex_wait(int *pointer, int expected, const struct timespec *timeout) { STUB("wait"); }
-  int sys_futex_wake(int *pointer) { STUB("wake"); }
-  int sys_isatty(int fd) { return fioxa_isatty(fd); }
-  int sys_open(const char* path, int flags, mode_t mode, int *fd) {
+  int Sysdeps<Close>::operator()(int fd) { return fioxa_close(fd); }
+  void Sysdeps<Exit>::operator()(int status) { fioxa_exit(status); }
+  int Sysdeps<FutexWait>::operator()(int *pointer, int expected, const struct timespec *timeout) { STUB("wait"); }
+  int Sysdeps<FutexWake>::operator()(int *pointer, bool all) { STUB("wake"); }
+  int Sysdeps<Isatty>::operator()(int fd) { return fioxa_isatty(fd); }
+  int Sysdeps<Open>::operator()(const char* path, int flags, mode_t mode, int *fd) {
     return fioxa_open(path, flags, mode, fd);
   }
-  int sys_read(int fd, void *buf, size_t count, ssize_t *bytes_read) {
+  int Sysdeps<Read>::operator()(int fd, void *buf, size_t count, ssize_t *bytes_read) {
     return fioxa_read(fd, buf, count, bytes_read);
   }
-  int sys_seek(int fd, off_t offset, int whence, off_t *new_offset) {
+  int Sysdeps<Seek>::operator()(int fd, off_t offset, int whence, off_t *new_offset) {
     return fioxa_seek(fd, offset, whence, new_offset);
   }
-  int sys_sleep(time_t *secs, long *nanos) {
+  int Sysdeps<Sleep>::operator()(time_t *secs, long *nanos) {
     return fioxa_sleep(*secs, *nanos);
   }
-  int sys_tcb_set(void *pointer) { fioxa_set_fs_reg(pointer); return 0; }
-  int sys_vm_map(void *hint, size_t size, int prot, int flags, int fd, off_t offset, void **window) {
+  int Sysdeps<TcbSet>::operator()(void *pointer) { fioxa_set_fs_reg(pointer); return 0; }
+  int Sysdeps<VmMap>::operator()(void *hint, size_t size, int prot, int flags, int fd, off_t offset, void **window) {
     if (fd != -1) return -ENOSYS;
     return fioxa_map(size, window);
   }
-  int sys_vm_unmap(void *pointer, size_t size) { return fioxa_unmap(pointer, size); }
-  int sys_write(int fd, const void *buf, size_t count, ssize_t *bytes_written) {
+  int Sysdeps<VmUnmap>::operator()(void *pointer, size_t size) { return fioxa_unmap(pointer, size); }
+  int Sysdeps<Write>::operator()(int fd, const void *buf, size_t count, ssize_t *bytes_written) {
     int ret = fioxa_write(fd, buf, count);
     *bytes_written = count;
     return ret;
   }
 
-  int sys_anon_allocate(size_t size, void **pointer) {
-    return sys_vm_map(NULL, size, PROT_READ | PROT_WRITE, MAP_ANON, -1, 0, pointer);
-  }
+  int Sysdeps<AnonAllocate>::operator()(size_t size, void **pointer) { return fioxa_map(size, pointer); }
 }
